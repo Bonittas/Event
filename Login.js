8cbcf86caac921e90d4cd85b74707ca105401c69
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-03-25 20:59:33 +0000

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,16 @@
+
+
+function Login(){
+    return(
+        <>
+       < div className="form">
+       <input type="text" value="User Name">
+        Email</input>   
+        <input type="text"value="Enter Password">
+        Enter Password</input>   
+        <button type="button" >Login</button>  
+       </div>
+        </>
+    )
+}
+export default Login;
