c8818bbcca69ab45ba1c8bca9df2d6cb37d5be34
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-03-25 20:59:55 +0000

    Add files via upload

---
+++ Signup.js
@@ -0,0 +1,16 @@
+function Signup(){
+    return(
+        <>
+       <div>
+       <input  value="User Name">
+        Email</input>   
+        <input  value="Enter Password">
+        Enter Password</input>
+        <input  value="Conform Password">
+        Conform Password</input>     
+        <button type="button">Signup</button>  
+       </div>
+        </>
+    )
+}
+export default Signup;
