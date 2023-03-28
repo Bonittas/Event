d0ca5775a1907e569a2a0d599680e37aec57d2cf
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-03-28 22:01:01 +0100

    Add files via upload

---
+++ db/conn.js
@@ -0,0 +1,17 @@
+const mysql = require("mysql2");
+
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database:"userimgupload"
+
+});
+
+conn.connect((error)=>{
+    if(error) throw error;
+    console.log("connected !")
+});
+
+module.exports = conn
