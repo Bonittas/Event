fea757acb18d9414d2e857c0676f37bc962106a7
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-03-28 21:59:25 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const express = require("express");
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
 
-const PORT = process.env.PORT || 3002;
-const app = express();
-app.get("/api", (req, res) => {
-    res.json({ message: "Hello from serve!" });
-  });
+import Register from './components/Register';
+import Home from './components/Home';
+import Signup from './components/Signup';
+import Header from './components/Header';
 
+import Login from './components/Login';
+export default function App() {
 
+  return (
+    <BrowserRouter>
+    <Routes>
+    
+      <Route path="/header" element={<Header />}/>
+        <Route index element={<Home />} />
+     
+        <Route path="/Signup" element={<Signup />} />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+     
+   
+    </Routes>
+  </BrowserRouter>
+  );}
 
-app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
-});
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App/>);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
