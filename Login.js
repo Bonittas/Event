fa61109f5aacf1201c26aeed9faa9083e54e277f
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-03-28 21:55:44 +0100

    Add files via upload

--- Login.js
+++ Login.js
@@ -1,15 +1,43 @@
-
-
+import axios from "axios";
+import { useState } from "react";
+import React from "react";
+import Header from "./Header";
 function Login(){
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState ("");
+  
+  const [loginStatus, setLoginStatus] = useState("");
+
+  const login = () => {
+    axios.post("http://localhost:3001/login", {
+       username: username,
+       password: password,
+    }).then((response) => {
+       if (!response.data.message) {
+          setLoginStatus( response.data.message);
+       } else {
+          setLoginStatus (response.data[0].message);
+       }
+    });
+    };
     return(
         <>
-       < div className="form">
-       <input type="text" value="User Name">
-        Email</input>   
-        <input type="text"value="Enter Password">
-        Enter Password</input>   
-        <button type="button" >Login</button>  
-       </div>
+        <Header/>
+       <form>
+      <label>Enter your name:
+        <input type="text" width={80} className="inpout" 
+         onChange = { (e) => {
+          setUsername (e.target.value);}}/>
+      </label>
+      <label>Enter password:
+        <input type="text" width={80} className="inpout"   onChange = { (e) => {
+          setPassword (e.target.value);}} />
+      </label>
+      <button type="button" className="btn"><a href="/">Login</a></button>
+    </form>
+      
+     
         </>
     )
 }
