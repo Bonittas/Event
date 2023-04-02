d9b868b1e21488ad214cbabb774d7236adb66a2f
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-04-02 21:53:22 +0100

    Update Login.js

--- Login.js
+++ Login.js
@@ -1,44 +1,62 @@
-import axios from "axios";
 import { useState } from "react";
-import React from "react";
-import Header from "./Header";
-function Login(){
-
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState ("");
-  
-  const [loginStatus, setLoginStatus] = useState("");
-
-  const login = () => {
-    axios.post("http://localhost:3001/login", {
-       username: username,
-       password: password,
-    }).then((response) => {
-       if (!response.data.message) {
-          setLoginStatus( response.data.message);
-       } else {
-          setLoginStatus (response.data[0].message);
-       }
-    });
-    };
-    return(
-        <>
-        <Header/>
-       <form>
-      <label>Enter your name:
-        <input type="text" width={80} className="inpout" 
-         onChange = { (e) => {
-          setUsername (e.target.value);}}/>
-      </label>
-      <label>Enter password:
-        <input type="text" width={80} className="inpout"   onChange = { (e) => {
-          setPassword (e.target.value);}} />
-      </label>
-      <button type="button" className="btn"><a href="/">Login</a></button>
-    </form>
-      
-     
-        </>
-    )
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import Validation from "./LoginValidation";
+import '../index.css'
+function Login() {    
+    const [values, setValues] = useState({       
+         email: '',       
+          password: ''    })   
+           const navigate = useNavigate();
+    const [errors, setErrors] = useState({})
+    const [backendError, setBackendError] = useState([])
+    const handleInput = (event) => {  
+    setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))    }   
+     const handleSubmit =(event) => {       
+         event.preventDefault();       
+          setErrors(Validation(values));        
+          if(errors.email === "" && errors.password === "") 
+          {            
+            axios.post('http://localhost:8081/login', values)           
+           .then(res => {                
+            if(res.data.errors) {                   
+             setBackendError(res.data.errors);                
+            } else {                    
+                setBackendError([]);                    
+                if(res.data === "Success") {                        
+                    navigate('/');                    
+                } else {                        
+                    alert("No record existed");                    
+                }                
+            }                            
+        })            
+        .catch(err => console.log(err));        
+    }    
 }
+  return (    <div className='login'>        
+  <div className='log'>           
+          
+   {                
+   backendError ? backendError.map( e => (                    
+   <p className='text-danger'>{e.msg}</p>                 
+   )) : <span></span>            
+   }            
+   <form action="" onSubmit={handleSubmit}>               
+    <div className='mb-3'>                    
+    <label htmlFor="email"><strong>Email</strong></label>                   
+     <input type="email" placeholder='Enter Email' name='email'                    
+     onChange={handleInput} className='form-control rounded-0'/>                   
+      {errors.email && <span className='text-danger'> {errors.email}</span>}                
+      </div>                <div className='mb-3'>                    
+      <label htmlFor="password"><strong>Password</strong></label>                   
+       <input type="password" placeholder='Enter Password' name='password'                   
+        onChange={handleInput} className='form-control rounded-0'/>                    
+        {errors.password && <span className='text-danger'> {errors.password}</span>}                
+        </div>                <button type='submit' className='btn1'> Log in</button>               
+         <p>You are agree to aour terms and policies</p>               
+          <Link to="/signup" className='btn2'>Create Account</Link>            
+          </form>        
+          </div>    
+          </div>  )}
 export default Login;
