2adbaf330cdabc3d5d686cdad274f9312619b6f1
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-05-01 22:01:44 +0300

    Add files via upload

---
+++ client/src/components/Sugg.js
@@ -0,0 +1,17 @@
+import './index.css'
+import Header from './components/Header';
+function Sugg(){
+   return(
+   
+  <div className="Sugg">
+     <Header/>
+     <div className='Events'>
+     <img  src='concert2.png'/>
+ <p>
+
+ </p></div>
+
+  </div>
+   ) 
+}
+export default Sugg;
