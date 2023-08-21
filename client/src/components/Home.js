8400512a56a92572630102abdb65b9b5b2c08239
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-08-21 04:33:12 +0300

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,104 +1,105 @@
-import React, { useEffect, useState } from 'react'
-
-
+import React, { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import axios from 'axios';
-
-import Alert from 'react-bootstrap/Alert';
-import "../index.css"
-
-import Admin from './Admin';
-import Footer from './Footer';
+import moment from 'moment';
 import Header from './Header';
 
-
 const Home = () => {
-  
-    const [data, setData] = useState([]);
+  const [data, setData] = useState([]);
+  const [show, setShow] = useState(false);
 
-    const [show, setShow] = useState(false);
+  const getUserData = async () => {
+    try {
+      const res = await axios.get('/getdata', {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-    const getUserData = async () => {
-        const res = await axios.get("/getdata", {
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        if (res.data.status == 201) {
-            console.log("data get");
-            setData(res.data.data)
-
-        } else {
-            console.log("error")
-        }
+      if (res.data.status === 201) {
+        console.log('data get');
+        setData(res.data.data);
+      } else {
+        console.log('error');
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  const dltUser = async (id) => {
+    try {
+      console.log(id);
+      const res = await axios.delete(`http://localhost:8007/${id}`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-   
+      if (res.data.status === 201) {
+        getUserData();
+        setShow(true);
+      } else {
+        console.log('error');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    useEffect(() => {
-        getUserData()
-    }, [])
+  useEffect(() => {
+    getUserData();
+  }, []);
 
-    return (
-         
-        <><Header/>
+  return (
+    <>
+    <Header/>
+      {show && (
+        <div className="fixed top-0 right-0 p-4">
+          <div className="bg-red-500 text-white rounded-md p-2 flex items-center">
+            <p className="mr-2">User Delete</p>
+            <button className="text-white" onClick={() => setShow(false)}>
+              Close
+            </button>
+          </div>
+        </div>
+      )}
 
+      <div className="container mx-auto  mt-2">
+        <h1 className="text-center mt-2 text-3xl text-white absolute top-64 font-bold">
+          Image Upload Projects With Mysql database
+        </h1>
 
-
-          
-          <div class="containers">
-  <div class="row">
-    <div class="col-md-12 text-center">
-      <h3 class="animate-charcter"> View events,  Add your events,  Enjoy moments</h3>
-    </div>
-  </div>
-</div>
-<div className='container'>
-    <div className='wrapper'>
-   
-  
-   </div>
-</div>
-            {
-                show ? <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-                    {/* User Delete */}
-                </Alert> : ""
-            }
-            <div className="container1">
-              
-                <div className='text2'>
-                    {
-                        data.length > 0 ? data.map((el, i) => {
-                          
-                            return (
-                                
-                                <>
-                                     <section className="Events">
-                                        
-                                     
-  <a className="Eimage" > <img id="ro" src={`/uploads/${el.userimg}`} height={200} width={250}/></a>
-
-<div className='container'>
-
-  <div className="discription">
-         
-              <p className="mytitle">Description : {el.username}</p></div>
-            
-
-      </div>
-     
- </section>
-                                </>
-                            )
-                        }) : ""
-                    }
-
+        <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-24 mt-5">
+          {data.length > 0 &&
+            data.map((el) => (
+              <div key={el.id} className="bg-white rounded-lg  shadow-md">
+                <div className=" mx-auto my-2 bg-purple-500 h-64 rounded-md overflow-hidden">
+                  <img
+                    src={`http://localhost:8007/uploads/${el.userimg}`}
+                    alt={el.username}
+                    className="object-cover w-full h-full"
+                  />
                 </div>
-            </div>
-            {/* <Footer/> */}
-        </>
-    )
-}
+                <div className="text-left p-2  ">
+                  <p className="text-lg font-bold text-center p-2">{el.username}</p>
+                  <p className="text-lg font-bold p-2">Location: {el.location}</p>
+                  <p className="text-lg font-bold p-2">Date: {moment(el.date).format('DD-MM-YYYY')}</p>
+                  <p className="text-lg font-bold p-2">
+                    Time: {moment(el.time, 'hh:mm:ss a').format('hh:mm a')}
+                  </p>
+                </div>
+                <div className="text-center mt-4">
+                  <button onClick={() => dltUser(el.id)} className="col-lg-6 text-center">
+                    Delete
+                  </button>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
