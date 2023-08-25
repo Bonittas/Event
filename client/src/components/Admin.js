e2eac1c8863ca10574172b839af52b0c96be7140
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-08-25 04:24:52 +0300

    Update Admin.js

--- client/src/components/Admin.js
+++ client/src/components/Admin.js
@@ -1,114 +1,47 @@
-// import React, { useEffect, useState } from 'react'
-// import Button from 'react-bootstrap/Button';
-// import { NavLink } from "react-router-dom"
-// import Card from 'react-bootstrap/Card';
-// import axios from 'axios';
-// import moment from "moment"
-// import Alert from 'react-bootstrap/Alert';
-// import "../index.css"
+import React, { useState, useEffect } from 'react';
+import Header2 from './Header2';
 
-// import Header from './Header';
-// // import SuggAdin from './SuggAdin';
+function Admin() {
+  const [contactInfo, setContactInfo] = useState([]);
 
-// const Admin = () => {
-  
-//     const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchContactInfo = async () => {
+      try {
+        const response = await fetch('/admin');
+        const data = await response.json();
+        setContactInfo(data);
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
 
-//     const [show, setShow] = useState(false);
+    fetchContactInfo();
+  }, []);
 
-//     const getUserData = async () => {
-//         const res = await axios.get("/getdata", {
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         });
+  return (
+    <>
+    <Header2/>
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4 ">Contact Information</h1>
+      {contactInfo.length > 0 ? (
+        <ul className="grid  gap-4">
+          {contactInfo.map((info) => (
+            <li
+              key={info.id}
+              className="border border-gray-300 w-1/2 bg-purple-100 rounded  p-4  shadow-md"
+            >
+              <div className="font-bold text-purple-950 mb-2">Name of User: {info.name}</div>
+              <div className="font-bold text-purple-950 mb-2">Email: {info.email}</div>
+              <div className="text-purple-950">Message: {info.message}</div>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div className="text-gray-600">No contact information available.</div>
+      )}
+    </div>
+    </>
+  );
+}
 
-//         if (res.data.status == 201) {
-//             console.log("data get");
-//             setData(res.data.data)
-
-//         } else {
-//             console.log("error")
-//         }
-//     }
-
-
-//     const dltUser = async (id) => {
-//         console.log(id)
-//         const res = await axios.delete(`/${id}`, {
-//             headers: {
-//                 "Content-Type": "application/json"
-//             }
-//         });
-
-//         if (res.data.status == 201) {
-//             getUserData()
-//             setShow(true)
-//         } else {
-//             console.log("error")
-//         }
-//     }
-
-//     useEffect(() => {
-//         getUserData()
-//     }, [])
-
-//     return (
-         
-//         <>
-//           <Header/>
-//       <button><a href='/register'>Adminsugg</a></button>
-//           <div class="containers">
-//   <div class="row">
-//     <div class="col-md-12 text-center">
-//       <h3 class="animate-charcter"> View events,  Add your events,  Enjoy moments</h3>
-//     </div>
-//   </div>
-// </div>
-//             {
-//                 show ? <Alert variant="danger" onClose={() => setShow(false)} dismissible>
-//                     {/* User Delete */}
-//                 </Alert> : ""
-//             }
-//             <div className="container1">
-              
-//                 <div className='text2'>
-//                     {
-//                         data.length > 0 ? data.map((el, i) => {
-                          
-//                             return (
-                                
-//                                 <>
-                             
-//                                      <section className="Events">
-                                     
-//   <a className="Eimage" > <img id="ro" src={`/uploads/${el.userimg}`} height={200} width={357}/></a>
-  
-// <div className='container'>
-
-//   <div className="discription">
-         
-//               <p className="mytitle">Description : {el.username}</p></div>
-//               <div className="add-img">
-//       <div className="del">
-//           <button type="button" id='del'  onClick={() => dltUser(el.id)}>
-//           Delete
-//               <span className="list"></span>
-//           </button>
-//       </div>
-//   </div>
-//       </div>
-     
-//  </section>
-//                                 </>
-//                             )
-//                         }) : ""
-//                     }
-
-//                 </div>
-//             </div>
-//         </>
-//     )
-// }
-
-// export default Admin
+export default Admin;
