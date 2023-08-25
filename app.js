e3a0f41617878e8a8d9f038cd02a9e15112f0a92
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-08-26 02:20:56 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,23 +14,26 @@ const conn = mysql.createConnection({
 
 conn.connect((error) => {
   if (error) throw error;
-  console.log('connected!');
+  console.log('Connected to the database!');
 });
 
+// img storage configuration
 const imgconfig = multer.diskStorage({
   destination: (req, file, callback) => {
     callback(null, './uploads');
   },
   filename: (req, file, callback) => {
-    callback(null, `image-${Date.now()}.${file.originalname}`);
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    callback(null, `image-${uniqueSuffix}.${file.originalname}`);
   },
 });
 
+// img filter
 const isImage = (req, file, callback) => {
   if (file.mimetype.startsWith('image')) {
     callback(null, true);
   } else {
-    callback(null, Error('only image is allowed'));
+    callback(Error('Only image files are allowed'));
   }
 };
 
@@ -45,70 +48,65 @@ app.use('/uploads', express.static('./uploads'));
 
 // register userdata
 app.post('/register', upload.single('photo'), (req, res) => {
-  const { topics } = req.body;
+  const { topics, locations, date, time, closingDate, closingTime } = req.body;
   const { filename } = req.file;
-  const { locations } = req.body;
-  const { date } = req.body;
-  const { time } = req.body;
 
-  if (!topics || !filename || !locations || !date || !time) {
-    res.status(422).json({ status: 422, message: 'fill all the details' });
+  if (!topics || !locations || !date || !time || !closingDate || !closingTime || !filename) {
+    return res.status(422).json({ status: 422, message: 'Please fill all the details' });
   }
 
-  try {
-    const date = moment(new Date()).format('YYYY-MM-DD');
+  const formattedDate = moment(date, 'YYYY-MM-DD').format('YYYY-MM-DD');
+  const formattedClosingDate = moment(closingDate, 'YYYY-MM-DD').format('YYYY-MM-DD');
 
-    conn.query(
-      'INSERT INTO usersdata SET ?',
-      { username: topics, userimg: filename, date: date, location: locations, time: time },
-      (err, result) => {
-        if (err) {
-          console.log('error');
-        } else {
-          console.log('data added');
-          res.status(201).json({ status: 201, data: req.body });
-        }
-      }
-    );
-  } catch (error) {
-    res.status(422).json({ status: 422, error });
-  }
+  const userData = {
+    username: topics,
+    userimg: filename,
+    date: formattedDate,
+    location: locations,
+    time: time,
+    closingDate: formattedClosingDate,
+    closingTime: closingTime,
+  };
+
+  conn.query('INSERT INTO usersdata SET ?', userData, (err, result) => {
+    if (err) {
+      console.error('Error inserting data:', err);
+      return res.status(500).json({ status: 500, message: 'Failed to insert data' });
+    }
+    
+    console.log('Data added successfully');
+    return res.status(201).json({ status: 201, data: userData });
+  });
 });
 
 // get user data
 app.get('/getdata', (req, res) => {
-  try {
-    conn.query('SELECT * FROM usersdata', (err, result) => {
-      if (err) {
-        console.log('error');
-      } else {
-        console.log('data get');
-        res.status(201).json({ status: 201, data: result });
-      }
-    });
-  } catch (error) {
-    res.status(422).json({ status: 422, error });
-  }
+  conn.query('SELECT * FROM usersdata', (err, result) => {
+    if (err) {
+      console.error('Error retrieving data:', err);
+      return res.status(500).json({ status: 500, message: 'Failed to retrieve data' });
+    }
+
+    console.log('Data retrieved successfully');
+    return res.status(200).json({ status: 200, data: result });
+  });
 });
 
 // delete user
 app.delete('/:id', (req, res) => {
   const { id } = req.params;
-  try {
-    conn.query(`DELETE FROM usersdata WHERE id ='${id}'`, (err, result) => {
-      if (err) {
-        console.log('error');
-      } else {
-        console.log('data delete');
-        res.status(201).json({ status: 201, data: result });
-      }
-    });
-  } catch (error) {
-    res.status(422).json({ status: 422, error });
-  }
+  conn.query(`DELETE FROM usersdata WHERE id = ?`, [id], (err, result) => {
+    if (err) {
+      console.error('Error deleting data:', err);
+      return res.status(500).json({ status: 500, message: 'Failed to delete data' });
+    }
+
+    console.log('Data deleted successfully');
+    return res.status(200).json({ status: 200, data: result });
+  });
 });
 
 const port = 8007;
 app.listen(port, () => {
-  console.log('server start');
+  console.log(`Server started on port ${port}`);
 });
