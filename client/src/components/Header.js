9320103b2dc5d4b3a79748c1abd67d0201d1d562
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-09-15 01:20:03 +0300

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
-import myImage from '../img/dg.avif';
+import myImage from '../img/s/events/dg.avif';
 
 function Header({ handleMenuToggle, isMenuOpen }) {
   return (
