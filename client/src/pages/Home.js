726a550fff67099b798c9b01c86f728b113c79c8
BIftu Shibbire <109244924+Bonittas@users.noreply.github.com>
2023-09-17 01:16:52 +0300

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -97,10 +97,13 @@ const Home = () => {
                 />
                 <button
                   type="submit"
-                  className="bg-purple-950 hover:bg-purple-700 text-white font-bold lg:h-16 md:h-16 sm:h-10 py-2 px-4 rounded-r-full absolute right-0 top-0"
+                  className="bg-purple-950 hover:bg-purple-900 text-white font-bold lg:h-16 md:h-16 sm:h-10 py-2 px-4 rounded-r-full absolute right-0 top-0"
                   onClick={handleFormSubmit}
                 >
-                  <FontAwesomeIcon icon={faSearch} />
+                  <div className='whitespace-nowrap px-2'>Search<FontAwesomeIcon
+  icon={faSearch}
+  style={{ marginLeft: '0.5rem' }}
+/></div>
                 </button>
               </div>
             </div>
